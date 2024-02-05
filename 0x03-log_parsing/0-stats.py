0aadfa0306fb6e5c93a5e5b694e09dc15b0b26b9
XimeonLeo <ximeonleo@gmail.com>
2024-02-05 21:14:21 +0000

    second algorithm

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,42 +1,39 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
 """ This module contains a script that reads
     stdin line by line and computes metrics
-    """
+"""
 import sys
 
-possible_status_codes = [200, 301, 400, 401, 403, 404, 405, 500]
-line_num = 0
-status_codes_map = {}
+
+status_code = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
 total_file_size = 0
-
-
-def print_stats():
-    """prints out the statistics"""
-    print("File size: {}".format(total_file_size))
-    for status, count in sorted(status_codes_map.items()):
-        print("{}: {}".format(status, count))
-
+lines_read = 0
 
 try:
     for line in sys.stdin:
-        line_tokens = line.split()
-        try:
-            file_size = int(line_tokens[-1])
-            total_file_size += file_size
-            status_code = int(line_tokens[-2])
-            if status_code in possible_status_codes:
-                if status_code in status_codes_map:
-                    status_codes_map[status_code] += 1
-                else:
-                    status_codes_map[status_code] = 1
-        except ValueError:
-            pass
-        line_num += 1
-        if line_num % 10 == 0:
-            print_stats()
+        line_list = line.split(" ")
+        if len(line_list) > 4:
+            code = line_list[-2]
+            size = int(line_list[-1])
+            if code in status_code.keys():
+                status_code[code] += 1
+            total_file_size += size
+            lines_read += 1
 
-    if (line_num == 0) or (line_num % 10 != 0):
-        print_stats()
+        if lines_read == 10:
+            lines_read = 0
+            print('File size: {}'.format(total_file_size))
+            for key, value in sorted(status_code.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
 
-except (KeyboardInterrupt):
-    print_stats()
+except Exception as err:
+    pass
+
+finally:
+    print('File size: {}'.format(total_file_size))
+    for key, value in sorted(status_code.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
